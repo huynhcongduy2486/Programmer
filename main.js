afd137b240e6271afe6cd6de2fe0c72bb011e117
huynhcongduy2486 <huynhcongduy2408@gmail.com>
2026-04-25 20:47:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,18 +16,30 @@ sections.forEach(sec=>{
   sec.style.transition="all 0.8s ease";
 });
 ---Chuyen khoan---
-if (--timer < 0) {
-    clearInterval(interval);
-    display.textContent = "Hết hạn";
-    
-    // Làm mờ và ngăn chặn click
-    const qrLinks = document.querySelectorAll('.qr-card');
-    qrLinks.forEach(link => {
-        link.style.pointerEvents = "none"; // Không cho click
-        link.style.opacity = "0.3";       // Làm mờ
-    });
-    
-    alert("Phiên thanh toán đã hết hạn!");
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    let countdown = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(countdown);
+            display.textContent = "ĐÃ HẾT HẠN";
+            
+            // Khóa tất cả các link không cho bấm vào nữa
+            const links = document.querySelectorAll('.qr-link');
+            links.forEach(link => {
+                link.style.pointerEvents = "none"; // Không cho click
+                link.style.opacity = "0.2";       // Làm mờ hẳn đi
+            });
+            alert("Phiên thanh toán đã hết hạn, vui lòng tải lại trang!");
+        }
+    }, 1000);
 }
 
 window.onload = function () {
